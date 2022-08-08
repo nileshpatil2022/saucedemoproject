8337479da6af41345c6cea6e49997de575820c86
LENOVO <LENOVO@DESKTOP-S344IQT>
2022-08-08 19:58:01 +0530

    second time commit

--- src/main/java/demoPack/TestGitHub.java
+++ src/main/java/demoPack/TestGitHub.java
@@ -3,7 +3,9 @@ package demoPack;
 public class TestGitHub 
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("GitHub");
+		System.out.println("server");
 	}
 }
